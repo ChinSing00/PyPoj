850c5727fa8247523e254e707d4b4632ff2d3f35
ChinSing00 <1029056991@qq.com>
2018-03-13 10:16:21 +0800

    完成了使用bs4对Dragen.py的正则部分的替换

--- downevery/beauti_test.py
+++ downevery/beauti_test.py
@@ -1,3 +1,4 @@
+'''
 import socket 
 from selectors import DefaultSelector,EVENT_WRITE,EVENT_READ
 
@@ -51,17 +52,18 @@ if __name__ == "__main__":
 		crawler.fetch()
 	loop()
 
-
-
-
 '''
+
+
+
 from bs4 import BeautifulSoup
 from bs4 import element
 
 soup = BeautifulSoup(open('rrs.html',encoding = 'utf-8'),'lxml')
 
-aas = soup.find('div',id='content')
-print(aas.get_text())
-#soup = list(filter(lambda x: type(x) == element.Tag, soup))
-#for x in soup:
-	#print(str(x.get_text()),'\n'+'---------------'*8)'''
+#aas = soup.find('div',id='content')
+#print(aas.get_text())
+
+soup = list(filter(lambda x: type(x) == element.Tag, soup))
+for x in soup:
+	print(str(x.get_text()),'\n'+'---------------'*8)
--- downevery/test.py
+++ downevery/test.py
@@ -3,11 +3,12 @@ import re
 import time
 import BaseSpi
 from bs4 import BeautifulSoup
+from bs4 import element
 '''
 (本程序使用了python的协程来进行网页的爬取,因为没有加入代理功能模块所以对爬取网页的速度做出了限制
 强行阻塞了三秒才开始下一个url的爬取.如果加入了代理模块与消息队列模块,多线程模块将可以在最大带宽
 下快速地爬取资源)-------------------已修改为V2版本,使用了Beautiful　Ｓｏｕｐ　来匹配网页内容，但是有
-部分内容还是需要正则来匹配（获得全体的ｕｒｌ什么的　）。
+部分内容还是需要正则来匹配（获得全体的ｕｒｌ什么的--假的,我tm全用bs4了　）。
 
 '''
 #################################################################################################
@@ -17,56 +18,63 @@ from bs4 import BeautifulSoup
 2018-03-01-----TODO添加GUI界面 
 '''
 class NovelDown:
-	def __init__(self，ｕｒｌ):
-		self._1rule = '<a href=".*?/(\d*.html)">'
-	    self._2rule = '<a href="[\./]*(\d*.html)">'
+	def __init__(self,url):
+		#self._1rule = '<a href=".*?/(\d*.html)">'
+	    #self._2rule = '<a href="[\./]*(\d*.html)">'
 	    self.url = url
-	    slef.bookname = ''
+	    self.bookname = ''
 
-	def getDownload(slef.url):
+	def getDownload(self,url):
 	    bs = BaseSpi.basespi()
 	    try:
-	        content = bs.getPageAnt(url,)  
+	        content = bs.getPage(url)  
 	        soup = BeautifulSoup(content,'lxml')
-	        bookname =''''something'''
-	        con_text = soup.find('div',id='content')
+	        bookname =str(self.bookname)
+	        con_text = soup.find('div',id='htmlContent')
 	        title = soup.title
 	        with open(bookname+'.txt','a+',encoding = 'utf-8') as f:
 	            f.write('\t\t'+'--'*4+title.get_text()+'--'*4)
 	            f.write(con_text.get_text()+'\n')
 	            return None
-	    except urllib.request.URLError as e:
-	        if hasattr(e,"reason"):
-	                print("connet is lost ....",e.reason)
-	                return None
+	    except :
+	        return None
 
-	def consumer(slef):
+	def consumer(self):
 	    r = 1.0
 	    while True:
 	        n = yield r
 	        if not n:
 	            return
-	        print(str(round((r/n[1]),2)*100)+'%')
 	        self.getDownload(n[0])
+	        print(str(round((r/n[1]),2)*100)+'%')
 	        r+= 1
 	 
-	def produce(slef,con):
+	def produce(self,con):
 	    con.send(None)
 	    n = ''
+	    si = []
 	    bs = BaseSpi.basespi()
-	    si = bs._regx(_1rule,bs. (self.url).decode('gbk'))
-	    print(si)
-	    l = 0
-	    for i in range(0,len(si)):
-	        n = [url+str(si[i]),len(si)]
+	    #soup = BeautifulSoup(bs.getPage(url),'lxml')
+	    soup = BeautifulSoup(open('lst.html',encoding='utf-8'),'lxml')
+	    ls = soup.find('div',class_='book_list')
+	    self.bookname = soup.title.string.strip('- 小说在线阅读 - 村上春树作品集')	
+	    lls = list(filter(lambda x: x.name == 'ul' , ls))
+	    ap = lls[0].find_all('a')
+	    list(filter(lambda x:si.append(x['href']),ap))
+	    #print(bn)
+	    for i in si:
+	        n = [i,len(si)]
 	        con.send(n)
 	        time.sleep(3)    
 	    con.close()
 
-url = 'https://www.zwdu.com/book/23488/'
-con = consumer()
-print('start.....')
-produce(con)
+
+if __name__ == '__main__':
+	url = 'http://cscs.zuopinj.com/5368/'
+	my = NovelDown(url)
+	con = my.consumer()
+	print('start.....')
+	my.produce(con)
  
 
 ###############################################################################
